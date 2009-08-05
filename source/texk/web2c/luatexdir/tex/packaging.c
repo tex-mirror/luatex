tex-mirror/luatex

commit c295bb93efd75ee58b94fb4f5911e4774e8f97c4

new function pack_width()
git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2945 0b2b3880-5936-4365-a048-eb17d2e5a6bf

source/texk/web2c/luatexdir/tex/packaging.c
@@ -485,8 +485,8 @@ halfword hpack(halfword p, scaled w, int m)
                 }
             }
             f = font(p);
+            x += pack_width(hpack_dir, glyph_dir, p, true);
             if (is_rotated(hpack_dir)) {
-                x = x + glyph_height(p) + glyph_depth(p);
                 s = glyph_width(p) / 2;
                 if (s > h)
                     h = s;
@@ -495,7 +495,6 @@ halfword hpack(halfword p, scaled w, int m)
             } else
                 if (dir_opposite
                     (dir_tertiary[hpack_dir], dir_tertiary[box_dir(r)])) {
-                x = x + glyph_width(p);
                 s = glyph_depth(p);
                 if (s > h)
                     h = s;
@@ -503,7 +502,6 @@ halfword hpack(halfword p, scaled w, int m)
                 if (s > d)
                     d = s;
             } else {
-                x = x + glyph_width(p);
                 s = glyph_height(p);
                 if (s > h)
                     h = s;
@@ -525,9 +523,9 @@ halfword hpack(halfword p, scaled w, int m)
                    because it is a highly negative number. */
                 if ((type(p) == hlist_node) || (type(p) == vlist_node)) {
                     s = shift_amount(p);
+                    x += pack_width(hpack_dir, box_dir(p), p, false);
                     if (dir_orthogonal
                         (dir_primary[box_dir(p)], dir_primary[hpack_dir])) {
-                        x = x + height(p) + depth(p);
                         if ((width(p) / 2) - s > h)
                             h = (width(p) / 2) - s;
                         if ((width(p) / 2) + s > d)
@@ -535,20 +533,17 @@ halfword hpack(halfword p, scaled w, int m)
 
                     } else if ((type(p) == hlist_node)
                                && is_mirrored(hpack_dir)) {
-                        x = x + width(p);
                         if (depth(p) - s > h)
                             h = depth(p) - s;
                         if (height(p) + s > d)
                             d = height(p) + s;
                     } else {
-                        x = x + width(p);
                         if (height(p) - s > h)
                             h = height(p) - s;
                         if (depth(p) + s > d)
                             d = depth(p) + s;
                     }
                 } else {
-                    x = x + width(p);
                     if (type(p) >= rule_node)
                         s = 0;
                     else
@@ -606,7 +601,7 @@ halfword hpack(halfword p, scaled w, int m)
                 } else {
                     if ((subtype(p) == pdf_refxform_node)
                         || (subtype(p) == pdf_refximage_node)) {
-                        x = x + pdf_width(p);
+                        x += pdf_width(p);
                         s = 0;
                         if (pdf_height(p) - s > h)
                             h = pdf_height(p) - s;
@@ -618,7 +613,7 @@ halfword hpack(halfword p, scaled w, int m)
             case glue_node:
                 /* Incorporate glue into the horizontal totals */
                 g = glue_ptr(p);
-                x = x + width(g);
+                x += width(g);
                 o = stretch_order(g);
                 total_stretch[o] = total_stretch[o] + stretch(g);
                 o = shrink_order(g);
@@ -650,7 +645,7 @@ halfword hpack(halfword p, scaled w, int m)
                     do_subst_font(margin_char(p), font_expand_ratio);
                     width(p) = -char_pw(margin_char(p), subtype(p));
                 }
-                x = x + width(p);
+                x += width(p);
                 break;
             case kern_node:
                 if (subtype(p) == normal) {
@@ -670,10 +665,10 @@ halfword hpack(halfword p, scaled w, int m)
                                                 character(vlink(p)));
                     }
                 }
-                x = x + width(p);
+                x += width(p);
                 break;
             case math_node:
-                x = x + surround(p);
+                x += surround(p);
                 break;
             case disc_node:
                 if (m == subst_ex_font)
@@ -888,15 +883,14 @@ scaled_whd natural_sizes(halfword p, halfword pp)
     while (p != pp && p != null) {
         while (is_char_node(p)) {
             f = font(p);
+            siz.wd += pack_width(hpack_dir, glyph_dir, p, true);
             if (is_rotated(hpack_dir)) {
-                siz.wd += (glyph_height(p) + glyph_depth(p));
                 s = glyph_width(p) / 2;
                 if (s > siz.ht)
                     siz.ht = s;
                 if (s > siz.dp)
                     siz.dp = s;
             } else {
-                siz.wd += glyph_width(p);
                 s = glyph_height(p);
                 if (s > siz.ht)
                     siz.ht = s;
@@ -914,9 +908,9 @@ scaled_whd natural_sizes(halfword p, halfword pp)
             case unset_node:
                 if ((type(p) == hlist_node) || (type(p) == vlist_node)) {
                     s = shift_amount(p);
+                    siz.wd += pack_width(hpack_dir, box_dir(p), p, false);
                     if (dir_orthogonal
                         (dir_primary[box_dir(p)], dir_primary[hpack_dir])) {
-                        siz.wd += (height(p) + depth(p));
                         if ((width(p) / 2) - s > siz.ht)
                             siz.ht = (width(p) / 2) - s;
                         if ((width(p) / 2) + s > siz.dp)
@@ -924,20 +918,17 @@ scaled_whd natural_sizes(halfword p, halfword pp)
 
                     } else if ((type(p) == hlist_node)
                                && is_mirrored(hpack_dir)) {
-                        siz.wd += width(p);
                         if (depth(p) - s > siz.ht)
                             siz.ht = depth(p) - s;
                         if (height(p) + s > siz.dp)
                             siz.dp = height(p) + s;
                     } else {
-                        siz.wd += width(p);
                         if (height(p) - s > siz.ht)
                             siz.ht = height(p) - s;
                         if (depth(p) + s > siz.dp)
                             siz.dp = depth(p) + s;
                     }
                 } else {
-                    siz.wd += width(p);
                     if (type(p) >= rule_node)
                         s = 0;
                     else
@@ -1066,24 +1057,24 @@ halfword vpackage(halfword p, scaled h, int m, scaled l)
                     s = shift_amount(p);
                     if (dir_orthogonal
                         (dir_primary[box_dir(p)], dir_primary[box_dir(r)])) {
-                        x = x + d + (width(p) / 2);
+                        x += d + (width(p) / 2);
                         d = width(p) / 2;
                         if (depth(p) + height(p) + s > w)
                             w = depth(p) + height(p) + s;
                     } else if ((type(p) == hlist_node)
                                && is_mirrored(box_dir(p))) {
-                        x = x + d + depth(p);
+                        x += d + depth(p);
                         d = height(p);
                         if (width(p) + s > w)
                             w = width(p) + s;
                     } else {
-                        x = x + d + height(p);
+                        x += d + height(p);
                         d = depth(p);
                         if (width(p) + s > w)
                             w = width(p) + s;
                     }
                 } else {
-                    x = x + d + height(p);
+                    x += d + height(p);
                     d = depth(p);
                     if (type(p) >= rule_node)
                         s = 0;
@@ -1097,7 +1088,7 @@ halfword vpackage(halfword p, scaled h, int m, scaled l)
                 /* Incorporate a whatsit node into a vbox */
                 if ((subtype(p) == pdf_refxform_node)
                     || (subtype(p) == pdf_refximage_node)) {
-                    x = x + d + pdf_height(p);
+                    x += d + pdf_height(p);
                     d = pdf_depth(p);
                     s = 0;
                     if (pdf_width(p) + s > w)
@@ -1106,10 +1097,10 @@ halfword vpackage(halfword p, scaled h, int m, scaled l)
                 break;
             case glue_node:
                 /* Incorporate glue into the vertical totals */
-                x = x + d;
+                x += d;
                 d = 0;
                 g = glue_ptr(p);
-                x = x + width(g);
+                x += width(g);
                 o = stretch_order(g);
                 total_stretch[o] = total_stretch[o] + stretch(g);
                 o = shrink_order(g);
@@ -1121,7 +1112,7 @@ halfword vpackage(halfword p, scaled h, int m, scaled l)
                 }
                 break;
             case kern_node:
-                x = x + d + width(p);
+                x += d + width(p);
                 d = 0;
                 break;
             default:
@@ -1132,7 +1123,7 @@ halfword vpackage(halfword p, scaled h, int m, scaled l)
     }
     width(r) = w;
     if (d > l) {
-        x = x + d - l;
+        x += d - l;
         depth(r) = l;
     } else {
         depth(r) = d;

source/texk/web2c/luatexdir/tex/directions.c
@@ -147,3 +147,23 @@ void print_dir(int d)
     print_char(dir_names[dir_secondary[d]]);
     print_char(dir_names[dir_tertiary[d]]);
 }
+
+/**********************************************************************/
+
+scaled pack_width(int curdir, int pdir, halfword p, boolean isglyph)
+{
+    scaled wd = 0;
+    if (isglyph) {
+        if (dir_parallel(dir_secondary[curdir], dir_secondary[pdir]) ==
+            dir_orthogonal(dir_secondary[pdir], dir_tertiary[pdir]))
+            wd = glyph_width(p);
+        else
+            wd = glyph_depth(p) + glyph_height(p);
+    } else {                    /* hlist, vlist, image, form, rule */
+        if (dir_parallel(dir_secondary[pdir], dir_secondary[curdir]))
+            wd = width(p);
+        else
+            wd = depth(p) + height(p);
+    }
+    return wd;
+}

source/texk/web2c/luatexdir/tex/directions.h
@@ -105,4 +105,6 @@ extern void scan_direction(void);
 extern halfword do_push_dir_node(halfword p, halfword a);
 extern halfword do_pop_dir_node(halfword p);
 
+scaled pack_width(int curdir, int pdir, halfword p, boolean isglyph);
+
 #endif

source/texk/web2c/luatexdir/tex/linebreak.c
@@ -824,11 +824,7 @@ static void add_to_widths(halfword s, integer line_break_dir,
 {
     while (s != null) {
         if (is_char_node(s)) {
-            if (is_rotated(line_break_dir)) {
-                widths[1] += glyph_height(s) + glyph_depth(s);
-            } else {
-                widths[1] += glyph_width(s);
-            }
+            widths[1] += pack_width(line_break_dir, glyph_dir, s, true);
             if ((pdf_adjust_spacing > 1) && check_expand_pars(font(s))) {
                 set_prev_char_p(s);
                 add_char_stretch(widths[8], s);
@@ -838,11 +834,7 @@ static void add_to_widths(halfword s, integer line_break_dir,
             switch (type(s)) {
             case hlist_node:
             case vlist_node:
-                if (!(dir_orthogonal(dir_primary[box_dir(s)],
-                                     dir_primary[line_break_dir])))
-                    widths[1] += width(s);
-                else
-                    widths[1] += (depth(s) + height(s));
+                widths[1] += pack_width(line_break_dir, box_dir(s), s, false);
                 break;
             case kern_node:
                 if ((pdf_adjust_spacing > 1) && (subtype(s) == normal)) {
@@ -875,11 +867,7 @@ static void sub_from_widths(halfword s, integer line_break_dir,
     while (s != null) {
         /* @<Subtract the width of node |s| from |break_width|@>; */
         if (is_char_node(s)) {
-            if (is_rotated(line_break_dir)) {
-                widths[1] -= (glyph_height(s) + glyph_depth(s));
-            } else {
-                widths[1] -= glyph_width(s);
-            }
+            widths[1] -= pack_width(line_break_dir, glyph_dir, s, true);
             if ((pdf_adjust_spacing > 1) && check_expand_pars(font(s))) {
                 set_prev_char_p(s);
                 sub_char_stretch(widths[8], s);
@@ -889,11 +877,7 @@ static void sub_from_widths(halfword s, integer line_break_dir,
             switch (type(s)) {
             case hlist_node:
             case vlist_node:
-                if (!(dir_orthogonal(dir_primary[box_dir(s)],
-                                     dir_primary[line_break_dir])))
-                    widths[1] -= width(s);
-                else
-                    widths[1] -= (depth(s) + height(s));
+                widths[1] -= pack_width(line_break_dir, box_dir(s), s, false);
                 break;
             case kern_node:
                 if ((pdf_adjust_spacing > 1) && (subtype(s) == normal)) {
@@ -1905,9 +1889,8 @@ ext_do_line_break(boolean d,
                    `\.{\\parfillskip}' glue appears at the end of each paragraph; it is therefore
                    unnecessary to check if |vlink(cur_p)=null| when |cur_p| is a character node.
                  */
-                active_width[1] += (is_rotated(line_break_dir))
-                    ? (glyph_height(cur_p) + glyph_depth(cur_p))
-                    : glyph_width(cur_p);
+                active_width[1] +=
+                    pack_width(line_break_dir, glyph_dir, cur_p, true);
                 if ((pdf_adjust_spacing > 1) && check_expand_pars(font(cur_p))) {
                     set_prev_char_p(cur_p);
                     add_char_stretch(active_width[8], cur_p);
@@ -1933,10 +1916,7 @@ ext_do_line_break(boolean d,
             case hlist_node:
             case vlist_node:
                 active_width[1] +=
-                    (dir_orthogonal
-                     (dir_primary[box_dir(cur_p)], dir_primary[line_break_dir]))
-                    ? (depth(cur_p) + height(cur_p))
-                    : width(cur_p);
+                    pack_width(line_break_dir, box_dir(cur_p), cur_p, false);
                 break;
             case rule_node:
                 active_width[1] += width(cur_p);

source/texk/web2c/luatexdir/tex/texnodes.h
@@ -174,15 +174,15 @@ typedef enum {
 #  define synctex_line_kern(a) vlink((a)+3)
 
 #  define box_node_size 9
-#  define width(a)            varmem[(a+2)].cint
-#  define depth(a)            varmem[(a+3)].cint
-#  define height(a)           varmem[(a+4)].cint
+#  define width(a)            varmem[(a)+2].cint
+#  define depth(a)            varmem[(a)+3].cint
+#  define height(a)           varmem[(a)+4].cint
 #  define shift_amount(a)     vlink((a)+5)
 #  define box_dir(a)          vinfo((a)+5)
 #  define list_ptr(a)         vlink((a)+6)
 #  define glue_order(a)       subtype((a)+6)
 #  define glue_sign(a)        type((a)+6)
-#  define glue_set(a)         varmem[(a+7)].gr
+#  define glue_set(a)         varmem[(a)+7].gr
 #  define synctex_tag_box(a)  vinfo((a)+8)
 #  define synctex_line_box(a) vlink((a)+8)
 
@@ -568,9 +568,9 @@ destination |pdf_ann_left| and |pdf_ann_top| are used for some types of destinat
 #  define pdf_ann_right(a)     varmem[(a) + 4].cint
 #  define pdf_ann_bottom(a)    varmem[(a) + 5].cint
 
-#  define pdf_width(a)         varmem[(a) + 2].cint
-#  define pdf_height(a)        varmem[(a) + 3].cint
-#  define pdf_depth(a)         varmem[(a) + 4].cint
+#  define pdf_width(a)         width(a)
+#  define pdf_height(a)        height(a)
+#  define pdf_depth(a)         depth(a)
 
 #  define pdf_literal_data(a)  vlink((a)+2)
 #  define pdf_literal_mode(a)  type((a)+2)

commit 28f361f89ef7fa0955e07fc56fd05e0813dbe7c7

fix a bug introduced in r2945
git-svn-id: https://serveur-svn.lri.fr/svn/modhel/luatex/trunk@2958 0b2b3880-5936-4365-a048-eb17d2e5a6bf

source/texk/web2c/luatexdir/tex/packaging.c
@@ -506,6 +506,7 @@ halfword hpack(halfword p, scaled w, int m)
                 break;
             case rule_node:
             case unset_node:
+                x += width(p);
                 if (type(p) >= rule_node)
                     s = 0;
                 else
@@ -867,6 +868,7 @@ scaled_whd natural_sizes(halfword p, halfword pp)
                 break;
             case rule_node:
             case unset_node:
+                siz.wd += width(p);
                 if (type(p) >= rule_node)
                     s = 0;
                 else